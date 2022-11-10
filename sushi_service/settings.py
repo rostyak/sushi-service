b7ee3e05dde7fc4910d1f2271ba71189c29f1889
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-10 23:21:53 +0200

    Add user create/logout pages, change sidebar

--- sushi_service/settings.py
+++ sushi_service/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = "sushi.Cook"
 
+LOGIN_REDIRECT_URL = "/"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
