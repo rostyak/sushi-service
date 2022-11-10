2759d5b28bbfa93090c4de974f331bdc27667f0e
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-10 15:09:52 +0200

    Add cook to AUTH_USER_MODEL

--- sushi_service/settings.py
+++ sushi_service/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sushi'
 ]
+
+AUTH_USER_MODEL = "sushi.Cook"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
