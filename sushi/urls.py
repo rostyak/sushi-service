79a309045f865d68be4cc7055d38c925e4d22dd7
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-10 18:44:55 +0200

    Create cook list (view, url, template). Update templates: base.html, sidebar.html

--- sushi/urls.py
+++ sushi/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from sushi.views import index
+from sushi.views import index, CookListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("cooks/", CookListView.as_view(), name="cooks-view"),
 ]
 
 app_name = "sushi"
