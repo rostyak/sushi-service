34debcc60652343c17bb4902d62ab6315a6a9f40
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-10 20:52:00 +0200

    Create dish types list (view, url, template). Update sidebar.html

--- sushi/urls.py
+++ sushi/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from sushi.views import index, CookListView, DishListView
+from sushi.views import index, CookListView, DishListView, DishTypeListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("cooks/", CookListView.as_view(), name="cooks-list"),
     path("dishes/", DishListView.as_view(), name="dishes-list"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-types-list")
 ]
 
 app_name = "sushi"
