4ef7b9741a25e613cab2e46206c9c4ea4f20db8c
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-10 15:06:52 +0200

    Add all needed models

--- sushi/models.py
+++ sushi/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField()
+
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="cars")
+
+    def __str__(self):
+        return self.name
