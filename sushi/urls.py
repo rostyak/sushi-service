b8c6e1879f701e9bdf211505be32773d4b3e74ba
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-10 19:48:01 +0200

    Update incorrect name of views

--- sushi/urls.py
+++ sushi/urls.py
@@ -5,8 +5,8 @@ from sushi.views import index, CookListView, DishListView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("cooks/", CookListView.as_view(), name="cooks-view"),
-    path("dishes/", DishListView.as_view(), name="dishes-view")
+    path("cooks/", CookListView.as_view(), name="cooks-list"),
+    path("dishes/", DishListView.as_view(), name="dishes-list"),
 ]
 
 app_name = "sushi"
