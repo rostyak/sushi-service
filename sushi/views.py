1cdaf7e47c5f471f7eed51de196dcd97b2cbc404
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-10 17:08:02 +0200

    Create index view, sushi/urls.py and add it here, create template for index page

--- sushi/views.py
+++ sushi/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from sushi.models import Cook, Dish, DishType
+
+
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+    }
+
+    return render(request, "sushi/index.html", context=context)
