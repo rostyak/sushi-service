1b0ec7c1c9511613c0932b11143b69707bb32da4
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-11 20:39:37 +0200

    Create create/update/delete views for dishes

--- sushi/urls.py
+++ sushi/urls.py
@@ -7,7 +7,10 @@ from sushi.views import (
     CookDetailView,
     DishListView,
     DishDetailView,
-    DishTypeListView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView,
+    DishTypeListView
 )
 
 urlpatterns = [
@@ -16,6 +19,9 @@ urlpatterns = [
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
     path("dishes/", DishListView.as_view(), name="dishes-list"),
     path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
+    path("dishes/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dishes/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-types-list")
 ]
 
