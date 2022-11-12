12f735b9fcb024df978e6ce8dd93f74af09d9ca6
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-12 15:52:06 +0200

    Add crispy-forms to the project, change dish form to crispy-form

--- sushi_service/settings.py
+++ sushi_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     "sushi",
 ]
 
@@ -71,6 +72,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "sushi_service.wsgi.application"
 
